51e6a9ca1e3f31f01bc92634a5080f7e1adbfcf1
Ben White <xperierfx@gmail.com>
2023-07-27 10:42:50 +1200

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("Hello") + ", " + name;
+    return messages.getString("Hello") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
